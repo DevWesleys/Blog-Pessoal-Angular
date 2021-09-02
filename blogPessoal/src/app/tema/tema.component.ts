18a22137084843193a148549d4f1fab0a7fa1a59
DevWesleys <itsbaianoman@gmail.com>
2021-09-02 16:27:44 -0300

    Adicionando componentes temas-edit e tema-service

--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscriber } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,15 +11,33 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
+  
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
   constructor(
-    private router:Router
-  ) { }
+    private router:Router , private temaService:TemaService
+) { }
 
   ngOnInit() {
      if(environment.token ==''){
       this.router.navigate(['/entrar'])
      }
+     this.findAllTemas()
   }
 
+  findAllTemas(){
+
+    this.temaService.getAllTema().subscribe((resp:Tema[]) =>{
+this.listaTemas = resp
+    })
+  }
+cadastrarTema(){
+  this.temaService.postTema(this.tema).subscribe((resp:Tema)=>{
+    this.tema = resp
+    alert("Tema cadastrado")
+    this.findAllTemas()
+    this.tema = new Tema()
+  })
+}
 }
